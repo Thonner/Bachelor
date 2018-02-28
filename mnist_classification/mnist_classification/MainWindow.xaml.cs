6054ac5ca83d9b8656a788bfbae2880217562fea
Simon Andersen <simon.thye@gmail.com>
2018-02-28 12:27:23 +0100

    Added file dialog

--- mnist_classification/mnist_classification/MainWindow.xaml.cs
+++ mnist_classification/mnist_classification/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
+
 namespace mnist_classification
 {
     /// <summary>
@@ -25,19 +29,71 @@ namespace mnist_classification
             InitializeComponent();
         }
 
-        private void Load_cfg_Click(object sender, RoutedEventArgs e)
+        private void Load_cfg_Button_Click(object sender, RoutedEventArgs e)
         {
             //Load cfg file
             Console.WriteLine("Pressed load cfg");
 
+
+
             //Add the load file under here.
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+
+            fileDialog.Filter = "Cfg Files (.cfg)|*.cfg|All Files (*.*)|*.*";
+
+            fileDialog.FilterIndex = 1;
+
+
+            bool? userClickedOK = fileDialog.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == true)
+            {
+                // Open the selected file to read.
+                System.IO.Stream fileStream = fileDialog.OpenFile();
+
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                {
+                    // Read the first line from the file and write it to the console. No point.
+                    Console.WriteLine( reader.ReadLine());
+                }
+                fileStream.Close();
+            }
+
         }
 
-        private void Load_weights_Click(object sender, RoutedEventArgs e)
+        private void Load_weights_Button_Click(object sender, RoutedEventArgs e)
         {
             //Load Weights file
             Console.WriteLine("Pressed load weights");
 
+
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+
+            fileDialog.Filter = "Weight Files (.weights)|*.weights|All Files (*.*)|*.*";
+
+            fileDialog.FilterIndex = 1;
+
+
+            bool? userClickedOK = fileDialog.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == true)
+            {
+                // Open the selected file to read.
+                System.IO.Stream fileStream = fileDialog.OpenFile();
+
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                {
+                    // Read the first line from the file and write it to the console. No point.
+                    Console.WriteLine(reader.ReadLine());
+                }
+                fileStream.Close();
+            }
         }
     }
 }
